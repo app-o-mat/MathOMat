ce4d7a91f4259de7e6666942ac52a35215f0a8c3
Lou Franco <lfranco@greenwave-solutions.com>
2019-12-08 19:58:20 -0500

    A button for answers

---
+++ MathOMat/MathPong/View/AnswerButtonNode.swift
@@ -0,0 +1,44 @@
+//
+//  AnswerButtonNode.swift
+//  MathOMat
+//
+//  Created by Louis Franco on 12/8/19.
+//  Copyright © 2019 App-o-Mat. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class AnswerButtonNode: SKSpriteNode {
+    private let labelNode = SKLabelNode()
+    private let flipped: Bool
+
+    public var text: String? {
+        get { return labelNode.text }
+        set {
+            labelNode.text = newValue
+            updateText()
+        }
+
+    }
+    init(color: UIColor, size: CGSize, flipped: Bool) {
+        self.flipped = flipped
+        super.init(texture: nil, color: color, size: size)
+        addChild(labelNode)
+        labelNode.fontSize = size.height * 0.8
+        if flipped {
+            labelNode.zRotation = .pi
+        }
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    func updateText() {
+        if flipped {
+            labelNode.position = CGPoint(x: 0, y: labelNode.frame.size.height / 2.0)
+        } else {
+            labelNode.position = CGPoint(x: 0, y: -(labelNode.frame.size.height) / 2.0)
+        }
+    }
+}
