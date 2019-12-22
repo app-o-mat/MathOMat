8266cf142b0fa108ab058ea4fba6c7c71989c8cd
Lou Franco <lfranco@greenwave-solutions.com>
2019-12-22 07:37:31 -0500

    Move most functionality to GameOMatKit

--- MathOMat/MathPong/View/MathPongScene.swift
+++ MathOMat/MathPong/View/MathPongScene.swift
@@ -8,71 +8,9 @@
 
 import SpriteKit
 import GameplayKit
+import GameOMatKit
 
-enum Constants {
-    static let problemName = "problem"
-    static let playerLineName = ["player1line", "player2line"]
-    static let buttonLineName = ["button1line", "button2line"]
-
-    static let fontName = "Courier"
-
-    static let sideInset: CGFloat = 5
-    static let settingKey = (
-        backgroundIndex: "math-pong.settingKey.backgroundIndex",
-        currentOpIndex: "math-pong.settingKey.currentOpIndex",
-        numberOfPlayers: "math-pong.settingKey.numberOfPlayers")
-    static let smallButtonSize = CGSize(width: 64, height: 64)
-}
-
-enum GameState {
-    case waitingToStart
-    case running
-    case paused
-}
-
-protocol GameLogicDelegate: class {
-    var gameState: GameState { get }
-
-    func didGameOver()
-    func scene() -> SKScene
-}
-
-protocol GameLogicPlayers {
-    var players: [MathPongPlayer] { get }
-
-    func currentPlayerHits()
-    func currentPlayerMisses()
-}
-
-protocol GameLogic: SKPhysicsContactDelegate {
-    var delegate: GameLogicDelegate? { get set }
-    var currentOp: MathOperator { get set }
-
-    func reset()
-    func addBoardNodes()
-    func run()
-    func gameOver()
-    func removeAllNodes()
-    func getPlayers() -> GameLogicPlayers?
-}
-
-extension GameLogic {
-}
-
-class MathPongScene: SKScene {
-
-    var gameLogic: GameLogic {
-        didSet {
-            didSetGameLogic()
-        }
-    }
-
-    var startButton: ColorButtonNode?
-    var pauseButton: ColorButtonNode?
-    var themeButton: ColorButtonNode?
-    var resetButton: ColorButtonNode?
-
-    var playerButtons = [ColorButtonNode]()
+class MathPongScene: GameScene {
 
     var opButtons = [ColorButtonNode]()
     var currentOp: MathOperator = MathOperator.add {
@@ -81,159 +19,36 @@ class MathPongScene: SKScene {
         }
     }
 
-    var backgroundIndex = 0 {
-        didSet {
-            UserDefaults.standard.set(backgroundIndex, forKey: Constants.settingKey.backgroundIndex)
-            self.backgroundColor = AppColor.boardBackground[backgroundIndex]
-        }
-    }
-
-    var numberOfPlayers = 2 {
-        didSet {
-            didSetNumberOfPlayer()
-        }
-    }
-
-    var gameState = GameState.waitingToStart
-
-    override init(size: CGSize) {
-        self.backgroundIndex = UserDefaults.standard.integer(forKey: Constants.settingKey.backgroundIndex)
-
-        let opIndex = UserDefaults.standard.integer(forKey: Constants.settingKey.currentOpIndex)
+    init(size: CGSize) {
+        let opIndex = UserDefaults.standard.integer(forKey: MathSettingKey.currentOpIndex)
         self.currentOp = MathOperator.at(index: opIndex)
 
-        let storedNumberOfPlayers = UserDefaults.standard.integer(forKey: Constants.settingKey.numberOfPlayers)
-        self.numberOfPlayers = (storedNumberOfPlayers > 0) ? storedNumberOfPlayers : 2
-        self.gameLogic = (self.numberOfPlayers == 1) ? MathPongOnePlayer() : MathPongTwoPlayer()
-
-        super.init(size: size)
+        super.init(size: size, gameLogics:
+            [PongOnePlayer(generator: MathOperator.add.generator()),
+             PongTwoPlayer(generator: MathOperator.add.generator())])
 
         didSetCurrentOp()
-        didSetGameLogic()
-
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-        self.backgroundColor = AppColor.boardBackground[backgroundIndex]
-
-        subscribeToAppEvents()
     }
 
-    @available(*, unavailable)
-    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    override func didSetGameLogic() {
+        super.didSetGameLogic()
+        self.gameLogic.generator = self.currentOp.generator()
+    }
 
     private func didSetCurrentOp() {
-        UserDefaults.standard.set(currentOp.index(), forKey: Constants.settingKey.currentOpIndex)
+        UserDefaults.standard.set(currentOp.index(), forKey: MathSettingKey.currentOpIndex)
         resetOperatorButtons()
-        self.gameLogic.currentOp = self.currentOp
+        self.gameLogic.generator = self.currentOp.generator()
     }
 
-    private func didSetGameLogic() {
-        self.gameLogic.delegate = self
-        self.gameLogic.currentOp = self.currentOp
-        self.physicsWorld.contactDelegate = self.gameLogic
-    }
-
-    private func didSetNumberOfPlayer() {
-        UserDefaults.standard.set(numberOfPlayers, forKey: Constants.settingKey.numberOfPlayers)
-        resetPlayerButtons()
-        self.gameLogic.removeAllNodes()
-        self.gameLogic = (self.numberOfPlayers == 1) ? MathPongOnePlayer() : MathPongTwoPlayer()
-        createGameBoard()
-    }
-
-    // App Events
-    private func subscribeToAppEvents() {
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self,
-                                       selector: #selector(appBecameActive),
-                                       name: UIApplication.didBecomeActiveNotification, object: nil)
-        notificationCenter.addObserver(self,
-                                       selector: #selector(appResignActive),
-                                       name: UIApplication.willResignActiveNotification, object: nil)
-    }
-
-    @objc private func appBecameActive() {
-        if self.gameState != .waitingToStart {
-            pauseGame()
-            removeControlButtons()
-            addStartButton()
-        }
-    }
-
-    @objc private func appResignActive() {
-        if self.gameState != .waitingToStart {
-            pauseGame()
-        }
-    }
-
-    func startGame() {
-        createGameBoard()
-        addWaitingToStartButtons()
-    }
-
-    func addWaitingToStartButtons() {
-        removeControlButtons()
-        addStartButton()
-        addThemeButton()
+    override func addWaitingToStartButtons() {
+        super.addWaitingToStartButtons()
         addOperatorButtons()
-        addPlayerButtons()
     }
 
-    func createGameBoard() {
-        gameLogic.addBoardNodes()
-    }
-
-    func removeControlButtons() {
-        self.pauseButton?.removeFromParent()
-        self.pauseButton = nil
-        self.startButton?.removeFromParent()
-        self.startButton = nil
-        self.themeButton?.removeFromParent()
-        self.themeButton = nil
-        self.resetButton?.removeFromParent()
-        self.resetButton = nil
+    override func removeControlButtons() {
+        super.removeControlButtons()
         removeOperatorButtons()
-        removePlayerButtons()
-    }
-
-    func addStartButton() {
-        let button = ColorButtonNode(
-            color: AppColor.imageButtonBackground,
-            size: CGSize(width: 128, height: 128))
-        self.startButton = button
-        addChild(button)
-        button.texture = SKTexture(imageNamed: "play-button")
-        button.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        button.onTap = { [weak self] button in
-            self?.onStartTapped()
-        }
-    }
-
-    func addThemeButton() {
-        let button = ColorButtonNode(
-            color: AppColor.imageButtonBackground,
-            size: Constants.smallButtonSize)
-        self.themeButton = button
-        addChild(button)
-        button.texture = SKTexture(imageNamed: "theme-button")
-        button.position = CGPoint(x: self.size.width / 2 + 96 + 10, y: self.size.height / 2 + 32 + 64 + 15)
-        button.onTap = { [weak self] button in
-            guard let sself = self else { return }
-            sself.backgroundIndex =  (sself.backgroundIndex + 1) % AppColor.boardBackground.count
-        }
-    }
-
-    func addResetButton() {
-        let button = ColorButtonNode(
-            color: AppColor.imageButtonBackground,
-            size: Constants.smallButtonSize)
-        self.resetButton = button
-        addChild(button)
-        button.texture = SKTexture(imageNamed: "reset-button")
-        button.position = CGPoint(x: self.size.width / 2 + 96 + 10, y: self.size.height / 2)
-        button.onTap = { [weak self] button in
-            guard let sself = self else { return }
-            sself.reset()
-        }
     }
 
     func addOperatorButtons() {
@@ -258,7 +73,7 @@ class MathPongScene: SKScene {
     func addOperatorButton(opName: String, position: CGPoint, on: Bool) -> ColorButtonNode {
         let button = ColorButtonNode(
             color: AppColor.imageButtonBackground,
-            size: Constants.smallButtonSize)
+            size: Style.smallButtonSize)
         addChild(button)
         button.texture = SKTexture(imageNamed: "\(opName)-button-\(on ? "on" : "off")")
         button.position = position
@@ -268,104 +83,5 @@ class MathPongScene: SKScene {
             sself.currentOp = MathOperator.named(name: button.name) ?? MathOperator.add
         }
         return button
-    }
-
-    func addPlayerButtons() {
-        var startPos = CGPoint(x: self.size.width / 2 + 64 + 10 + 32,
-                               y: self.size.height / 2  - 5 - 32)
-        for (i, player) in ["1p", "2p"].enumerated() {
-            self.playerButtons.append(
-                addPlayerButton(name: player, position: startPos, on: numberOfPlayers == (i+1)))
-            startPos.y -= 64 + 10
-        }
-    }
-
-    func addPlayerButton(name: String, position: CGPoint, on: Bool) -> ColorButtonNode {
-        let button = ColorButtonNode(
-            color: AppColor.imageButtonBackground,
-            size: Constants.smallButtonSize)
-        addChild(button)
-        button.texture = SKTexture(imageNamed: "\(name)-button-\(on ? "on" : "off")")
-        button.position = position
-        button.name = name
-        button.onTap = { [weak self] _ in
-            guard let numberOfPlayers = Int(String(name.prefix(1))) else { return }
-            self?.numberOfPlayers = numberOfPlayers
-        }
-        return button
-    }
-
-    func resetPlayerButtons() {
-        removePlayerButtons()
-        addPlayerButtons()
-    }
-
-    func removePlayerButtons() {
-        self.playerButtons.forEach { $0.removeFromParent() }
-        self.playerButtons = []
-    }
-
-    func onStartTapped() {
-        if self.gameState == .paused {
-            unPauseGame()
-        } else if self.gameState == .waitingToStart {
-            createRunningGameBoard()
-        }
-        addPauseButton()
-    }
-
-    func addPauseButton() {
-        removeControlButtons()
-
-        let button = ColorButtonNode(
-            color: AppColor.imageButtonBackground,
-            size: CGSize(width: 128, height: 128))
-        self.pauseButton = button
-        addChild(button)
-        button.texture = SKTexture(imageNamed: "pause-button")
-        button.alpha = 0.4
-        button.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        button.onTap = { [weak self] button in
-            self?.onPauseTapped()
-        }
-    }
-
-    func onPauseTapped() {
-        pauseGame()
-        removeControlButtons()
-        addStartButton()
-        addResetButton()
-    }
-
-    func createRunningGameBoard() {
-        gameLogic.reset()
-        gameLogic.run()
-        self.gameState = .running
-    }
-
-    func reset() {
-        unPauseGame()
-        gameLogic.gameOver()
-    }
-
-    func pauseGame() {
-        self.gameState = .paused
-        self.isPaused = true
-    }
-
-    func unPauseGame() {
-        self.gameState = .running
-        self.isPaused = false
-    }
-}
-
-extension MathPongScene: GameLogicDelegate {
-    func didGameOver() {
-        addWaitingToStartButtons()
-        self.gameState = .waitingToStart
-    }
-
-    func scene() -> SKScene {
-        return self
     }
 }
---
+++ MathOMat/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MathOMat
+//
+//  Created by Louis Franco on 12/21/19.
+//  Copyright © 2019 App-o-Mat. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+enum MathSettingKey {
+    static let currentOpIndex = "math-pong.settingKey.currentOpIndex"
+}
